e7ff2873711e4af9f72813531560518e01583b36
AbdallaEssam <abdoTheKing@live.com>
2020-12-24 12:38:17 +0200

    First commit

---
+++ src/Banker/BankImpl.java
@@ -0,0 +1,28 @@
+package Banker;
+
+/**
+ * The Bank
+ */
+public class BankImpl implements Bank {
+
+    @Override
+    public void addCustomer(int threadNum, int[] maxDemand) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getState() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean requestResources(int threadNum, int[] request) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void releaseResources(int threadNum, int[] release) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
